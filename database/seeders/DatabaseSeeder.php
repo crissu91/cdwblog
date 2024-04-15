163c8a9092b5af53f607f832eb5b3c7322cd5fca
Cristina Stefan <cristinaf.stefan@gmail.com>
2024-04-15 14:51:17 +0100

    Add factories and seeding db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Article;
+use App\Models\Tag;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call(CategorySeeder::class);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory()->count(10)->create();
+
+        Article::factory()
+            ->has(Tag::factory()->count(2))
+            ->count(50)
+            ->create();
     }
 }
