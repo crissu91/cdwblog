abd6094e0ef0d802286e9a3bacb049e1a27cd203
Cristina Stefan <cristinaf.stefan@gmail.com>
2024-04-15 10:41:14 +0100

    Adds relationships

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Article extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name', 'excerpt', 'description', 'category_id', 'user_id', 'status'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
